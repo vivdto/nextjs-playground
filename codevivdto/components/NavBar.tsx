ecd60e7c71281793ab96ba3ef5d2f039f9d55bab
vivdto <vivdto@gmail.com>
2025-02-20 23:54:34 +0530

    Added sampleBlogs

--- codevivdto/components/NavBar.tsx
+++ codevivdto/components/NavBar.tsx
@@ -1,31 +1,89 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { ModeToggle } from './theme-toggle'
+import { HamburgerMenuIcon } from '@radix-ui/react-icons'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import LoadingBar from 'react-top-loading-bar'
+import { usePathname } from 'next/navigation'
+import MobileNav from './mobile-nav'
+import { Menu } from 'lucide-react'
+
 
 
 const NavBar = () => {
-  return (
-    <nav className='h-16 bg-background/60 sticky top-0 border-b px-8 backdrop-blur flex items-center'>
-      <div className='font-extrabold text-xl'>
-        .vivdtoSpace
-      </div>
-      <ul className='flex w-full justify-end space-x-4 items-center'>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li className='flex space-x-2'>
-          <Button variant="outline" asChild>
-            <Link href="/login">Login</Link>
-          </Button>
-          <Button variant="outline" asChild>
-            <Link href="/signup">Signup</Link>
-          </Button>
-          <ModeToggle/>
+    const [progress, setProgress] = useState(0)
+    const pathname = usePathname();
+    
+    // This runs whenever page changes to some other page
+    useEffect(() => {
+        setProgress(30)
+        
+        setTimeout(() => { 
+            setProgress(70)
+        }, 100);
 
-        </li>
-      </ul>
-    </nav>
-  )
+        setTimeout(() => { 
+            setProgress(100)
+        }, 800);
+       
+    }, [pathname])
+
+    // This runs whenever page loads
+    useEffect(() => {
+        setTimeout(() => { 
+            setProgress(0)
+        }, 900);
+    }, [])
+    
+ 
+
+    return (
+        <nav className='h-16 bg-background/50 sticky top-0 border-b px-8 backdrop-blur flex items-center justify-between z-10'>
+            <LoadingBar
+                color='#6028ff'
+                progress={progress}
+                onLoaderFinished={() => setProgress(0)}
+            />
+            <div className='text-lg font-bold md:text-xl'>
+                <Link href={"/"}>
+                    .vivdtoSpace
+                </Link>
+            </div>
+            <ul className='hidden md:flex w-full justify-end items-center space-x-4 '>
+                <li><Link href={"/"}>Home</Link></li>
+                <li><Link href={"/about"}>About</Link></li>
+                <li><Link href={"/blog"}>Blog</Link></li>
+                <li><Link href={"/contact"}>Contact</Link></li>
+                <li className="buttons px-4 space-x-2">
+                    <Link href={"/login"} className={buttonVariants({ variant: "outline" })}>Login</Link>
+                    <Link href={"/login"} className={buttonVariants({ variant: "outline" })}>Sign Up</Link>
+                </li>
+
+            </ul>
+            <ModeToggle />
+
+            <div className="flex items-center justify-center sm:hidden">
+
+              <Sheet>
+                <SheetTrigger><Menu /></SheetTrigger>
+                <SheetContent>
+
+                  <MobileNav />
+
+                </SheetContent>
+              </Sheet>
+            </div>
+        </nav>
+    )
 }
 
 export default NavBar
